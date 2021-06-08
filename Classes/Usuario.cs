4e1476eaab084091f568e37e3e6b9bc71b3ed245
Lucas Araujo <lucasaraujo4188@gmail.com>
2021-06-08 14:43:54 -0300

    Melhora a validação do usuário

--- Classes/Usuario.cs
+++ Classes/Usuario.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using System;
 using AulaPOO_ProjetoDeProdutos.Interfaces;
-using AulaPOO_ProjetoDeProdutos.Classes;
 namespace AulaPOO_ProjetoDeProdutos.Classes
 {
     public class Usuario : IUsuario
@@ -38,17 +37,25 @@ namespace AulaPOO_ProjetoDeProdutos.Classes
 
         public void validacao(string k, string q)
         {
-            Usuario logando = cadastros.Find(item => cadastros.Find(item => item.Email == k).Senha == q);
+            Usuario logando = cadastros.Find(item => item.Email == k);
 
             if (logando != null)
             {
-                login.Logar(logando);
-               
+                if (logando.Senha == q)
+                {
+                    login.Logar(logando);
+                }
+                else
+                {
+                    Console.WriteLine("Senha ou usuário incorreto. Tente novamente");
+                }
+
+
             }
             else
             {
                 Console.WriteLine("Senha ou usuário incorreto. Tente novamente");
-                
+
             }
         }
     }
